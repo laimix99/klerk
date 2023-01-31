a77adb34f2debf9f3131fa93ac55b868d18c5aa2
laimix99 <80690937+laimix99@users.noreply.github.com>
2023-01-31 14:58:19 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-full max-w-600px py-5px h-278 flex flex-col items-center br">
+    <div class=" h-4 bg">
+      <!-- <h1 class="text-10px">привет</h1> -->
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .br {
+    outline: 1px solid red;
+  }
+  .bg {
+    outline: 1px solid green;
+  }
+</style>
